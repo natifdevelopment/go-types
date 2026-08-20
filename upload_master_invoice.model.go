ef6bcbe08dc72933eeb449180c4aa3f844ff4a67
acep sopian <acep.sopian@iconpln.co.id>
2026-08-20 20:20:53 +0700

    feat(types): add SKB and SPTP fields to UploadMasterInvoice model

--- upload_master_invoice.model.go
+++ upload_master_invoice.model.go
@@ -1,7 +1,8 @@
 package types
+
 import (
+	"github.com/google/uuid"
 	baseapp "github.com/natifdevelopment/go-baseapp"
-"github.com/google/uuid"
 )
 
 func (UploadMasterInvoice) TableName() string {
@@ -10,14 +11,22 @@ func (UploadMasterInvoice) TableName() string {
 
 type UploadMasterInvoice struct {
 	baseapp.BaseModel
-	TOrganizationId  *uuid.UUID       `gorm:"type:uuid" json:"organizationId"`
-	Organization     *Organization    `gorm:"foreignKey:TOrganizationId" json:"organization"`
-	NoNpwp           string           `gorm:"type:varchar(255)" json:"noNpwp"`
-	TglTerdaftarNpwp *DateField `gorm:"type:date" json:"tglTerdaftarNpwp"`
-	TDokNpwpId       *uuid.UUID       `gorm:"type:uuid" json:"dokNpwpId"`
-	DokNpwp          *Media           `gorm:"foreignKey:TDokNpwpId" json:"dokNpwp"`
-	NoPkp            string           `gorm:"type:varchar(255)" json:"noPkp"`
-	TDokPkpId        *uuid.UUID       `gorm:"type:uuid" json:"dokPkpId"`
-	DokPkp           *Media           `gorm:"foreignKey:TDokPkpId" json:"dokPkp"`
-	StatusInvoice    bool             `gorm:"type:bool" json:"statusInvoice"`
+	TOrganizationId  *uuid.UUID    `gorm:"type:uuid" json:"organizationId"`
+	Organization     *Organization `gorm:"foreignKey:TOrganizationId" json:"organization"`
+	NoNpwp           string        `gorm:"type:varchar(255)" json:"noNpwp"`
+	TglTerdaftarNpwp *DateField    `gorm:"type:date" json:"tglTerdaftarNpwp"`
+	TDokNpwpId       *uuid.UUID    `gorm:"type:uuid" json:"dokNpwpId"`
+	DokNpwp          *Media        `gorm:"foreignKey:TDokNpwpId" json:"dokNpwp"`
+	NoPkp            string        `gorm:"type:varchar(255)" json:"noPkp"`
+	TDokPkpId        *uuid.UUID    `gorm:"type:uuid" json:"dokPkpId"`
+	DokPkp           *Media        `gorm:"foreignKey:TDokPkpId" json:"dokPkp"`
+	TglSkbAwal       *DateField    `gorm:"type:date" json:"tglSkbAwal"`
+	TglSkbAkhir      *DateField    `gorm:"type:date" json:"tglSkbAkhir"`
+	TDokSkbId        *uuid.UUID    `gorm:"type:uuid" json:"dokSkbId"`
+	DokSkb           *Media        `gorm:"foreignKey:TDokSkbId" json:"dokSkb"`
+	TglSptpAwal      *DateField    `gorm:"type:date" json:"tglSptpAwal"`
+	TglSptpAkhir     *DateField    `gorm:"type:date" json:"tglSptpAkhir"`
+	TDokSptpId       *uuid.UUID    `gorm:"type:uuid" json:"dokSptpId"`
+	DokSptp          *Media        `gorm:"foreignKey:TDokSptpId" json:"dokSptp"`
+	StatusInvoice    bool          `gorm:"type:bool" json:"statusInvoice"`
 }
